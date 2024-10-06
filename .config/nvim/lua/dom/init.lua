664922166745e6b6702e02984cdae63d4b14d980
IVESTAS <108275858+IVESTAS@users.noreply.github.com>
2024-10-06 17:48:31 +0000

    Delete .config/nvim directory

--- .config/nvim/lua/dom/init.lua
+++
@@ -1,3 +0,0 @@
-require("dom.remap")
-require("dom.set")
-require("dom.lazy_init")
--- .config/nvim/lua/dom/lazy/indent-line.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "Yggdroot/indentLine",
-    config = function()
-    vim.g.indentLine_leadingSpaceEnabled = 1
-    vim.g.indentLine_leadingSpaceChar = '·'
-    vim.g.indentLine_char_list = {'·'}
-
-end
-}

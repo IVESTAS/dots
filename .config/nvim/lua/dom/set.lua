cedb4b0e7f7bd37941b13ff488bee1466608e0b4
IVESTAS <108275858+IVESTAS@users.noreply.github.com>
2024-11-02 22:40:55 +0000

    Add files via upload

---
+++ .config/nvim/lua/dom/set.lua
@@ -0,0 +1,42 @@
+vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.syntax = off
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "no"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.rustfmt_autosave = 1
+vim.opt_local.formatprg = hindent
+vim.opt.splitbelow = true
+
+vim.g.mapleader = " "
+
+-- thanks primeagen
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+--vim.g.netrw_winsize = 25

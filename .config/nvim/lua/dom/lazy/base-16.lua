393ecce0c58ae21672dd2199119590454f3c899c
IVESTAS <108275858+IVESTAS@users.noreply.github.com>
2025-01-03 14:22:01 +0000

    Add files via upload

---
+++ .config/nvim/lua/dom/lazy/base-16.lua
@@ -0,0 +1,37 @@
+return {
+ --   "RRethy/nvim-base16",  -- The plugin declaration
+
+ --   -- Configuring the plugin
+ --   config = function()
+ --       -- Set up the base16 color scheme with your custom colors
+ --       require('base16-colorscheme').setup({
+ --           base00 = '#0A0A0A', -- background 161616
+ --           base01 = '#3a3a3a', -- word wrap 
+ --           base02 = '#1a1a1a', -- title bar
+ --           base03 = '#7a7a7a', -- comment
+ --           base04 = '#333333', -- Numbers
+ --           base05 = '#EFEFEF', -- parameter
+
+ --           base06 = '#b6db51',
+ --           base07 = '#b6db51',
+
+ --           base08 = '#efefef', -- variable (return)
+ --           base09 = '#b6db51', -- number
+ --           base0A = '#b6db51', -- keyword
+
+ --           base0B = '#b6db51', -- string
+ --           base0C = '#EFEFEF',
+
+ --           base0D = '#b6db51', -- function
+
+ --           base0E = '#b6db51', -- control flow
+ --           base0F = '#b6db51', -- \n
+ --       })
+
+ --       -- Apply the color scheme
+ --       --vim.cmd('colorscheme colorscheme')
+---- TODO: dsadas
+ --       -- Enable true color support (optional but recommended)
+ --       vim.o.termguicolors = true
+ --   end
+}

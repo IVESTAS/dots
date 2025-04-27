0156b72e6a56412ae93adfc9db50a1cd315934f6
IVESTAS <108275858+IVESTAS@users.noreply.github.com>
2025-04-27 01:28:47 +0000

    Add files via upload

---
+++ .config/nvim/lua/dom/lazy/lsp.lua
@@ -0,0 +1,52 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        local cmp = require('cmp')
+        require("mason").setup()
+        require("mason-lspconfig").setup()
+        cmp.setup({
+            sources = {
+                {
+                     name = "buffer" ,
+                     max_item_count = 3,
+                },
+            },
+            mapping = cmp.mapping.preset.insert({
+                --['<S-Tab>'] = cmp.mapping.confirm({ select = true}),
+                ['<C-Space>'] = cmp.mapping(cmp.mapping.select_next_item()),
+                ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item()),
+                --['<C-Space>'] = cmp.mapping.complete(),
+            }),
+        })
+        require('lspconfig').clangd.setup {
+            on_init = function(client, _)
+                client.server_capabilities.semanticTokensProvider = nil -- turn off semantic tokens
+            end,
+        }
+        require('lspconfig').ols.setup {
+            on_init = function(client, _)
+--                client.server_capabilities.semanticTokensProvider = nil -- turn off semantic tokens
+            end,
+        }
+        require('lspconfig').glsl_analyzer.setup {
+            on_init = function(client, _)
+                --client.server_capabilities.semanticTokensProvider = nil -- turn off semantic tokens
+            end,
+        }
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "cpp",
+            callback = function()
+                vim.opt_local.cindent = false
+            end,
+        })
+    end
+}
+

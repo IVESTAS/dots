96bb8c8d34b22592e7d5f50e72d9827e98c6dd81
IVESTAS <108275858+IVESTAS@users.noreply.github.com>
2025-04-27 01:28:21 +0000

    Add files via upload

--- .config/nvim/lua/dom/set.lua
+++ .config/nvim/lua/dom/set.lua
@@ -39,6 +39,34 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.o.cinoptions = "l1"
 --vim.o.cindent = false
+--
+
+-- Compile document, be it groff/LaTeX/markdown/etc.
+vim.keymap.set('n', '<leader>c', '<cmd>w! | !compiler "%:p"<CR>')
+
+-- Runs a script that cleans out tex build files whenever I close out of a .tex file.
+vim.api.nvim_create_autocmd("VimLeave", { pattern = "*.tex", command = "!latexmk -c %" })
+
+-- Lua function to open corresponding PDF in Zathura
+local function ZathuraOpenPdf()
+  local full_path = vim.fn.expand("%:p")
+  local pdf_file = full_path:gsub("%.tex$", ".pdf")
+  vim.fn.jobstart({ "zathura", pdf_file }, { detach = true })
+end
+
+-- Map Alt+p to call the Lua function
+vim.keymap.set("n", "<A-p>", ZathuraOpenPdf, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<A-m>", function()
+  vim.cmd("make")
+end, { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.tex",
+  callback = function()
+    vim.fn.jobstart("make", { detach = true })
+  end,
+})
 
 vim.opt.background = "dark"  -- or "dark" based on your preference
 vim.cmd("colorscheme habamax")

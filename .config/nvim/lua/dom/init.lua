36aa2b59ac3349db35a3895009a5bc0ebc3ae9fc
IVESTAS <108275858+IVESTAS@users.noreply.github.com>
2025-08-16 19:22:43 +0000

    Delete .config/nvim directory

--- .config/nvim/lua/dom/init.lua
+++
@@ -1,7 +0,0 @@
-require("dom.remap")
-require("dom.set")
-require("dom.lazy_init")
-
-local augroup = vim.api.nvim_create_augroup
-local domGroup = augroup('dom', {})
-local autocmd = vim.api.nvim_create_autocmd

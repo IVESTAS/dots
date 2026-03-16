c5b3a50d8cc81ab80a5fdd992cbff70910df89c3
IVESTAS <108275858+IVESTAS@users.noreply.github.com>
2026-03-16 08:28:04 +0000

    Add files via upload

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        --file_ignore_patterns = { "%.o", "%.so.3.4", "%.so", "%.out"},
+        preview = {
+          treesitter = false
+        }
+      },
+    })
+
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>;f', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>;s', builtin.live_grep, { desc = 'Telescope live grep' })
+  end
+}
